2ae919b97c332d713e895e8e7f0d2ed19c646d07
Lidija Levchenko <levchenkola@ukr.net>
2021-12-19 23:31:38 +0200

    +

---
+++ src/js/01-gallery.js
@@ -0,0 +1,9 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
