401f19ff85eb4a2f6a5cb5e0a77c6707aeaa41a8
Lidija Levchenko <levchenkola@ukr.net>
2021-12-23 23:04:53 +0200

    =

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+const iFrame = document.querySelector('#vimeo-player');
+import throttle from 'lodash.throttle';
 
+//const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iFrame);
+function savedCurrentTime(date) {
+    localStorage.setItem('videoplayer-current-time', Math.round(date.seconds));
+   
+}
+const throttleSavedCurrentTime = throttle(savedCurrentTime, 1000);
+player.on('timeupdate', throttleSavedCurrentTime );
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentTime).then(function (seconds) { })
+.catch(function (error) {
+switch (error.name) {
+    case 'RangeError':
+        break;
+    default:
+        break;
+}
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
-
+const throttle = require('lodash.throttle');
+const formREf = document.querySelector('.feedback-form');
+const emailREf = formREf.querySelector('input');
+const textREf = formREf.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+// ------------------------------------------------
+returnSevedMessage();
+// ------------------------------------------------
+function saveInputData(e) {
+  let feedbackFormState = {};
+  feedbackFormState[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
+}
+formREf.addEventListener('input', throttle(saveInputData, 500));
+// ------------------------------------------------
+formREf.addEventListener('submit', function (e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+});
+// ------------------------------------------------
+function returnSevedMessage() {
+  let parstLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (parstLocalStorage) {
+    parstLocalStorage.email ? (emailREf.value = parstLocalStorage.email) : (emailREf.value = ' ');
+    parstLocalStorage.message ? (textREf.value = parstLocalStorage.message) : (textREf.value = ' ');
+  }
+}
